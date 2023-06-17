deca2c28d5071a5ee4f040ba81b09bd4e212abf8
IdrisIsra <israpilov.idris@gmail.com>
2023-06-17 20:26:25 +0300

    added referrer validation through cookie

--- app/route.ts
+++ app/route.ts
@@ -3,14 +3,14 @@ import { redirect } from "next/navigation"
 
 export async function GET(request: Request) {
   const cookieStore = cookies()
+  const headersList = headers()
+
+  const referer = headersList.get("referer")
+  cookieStore.set("referer", referer ?? "")
+
   const token = cookieStore.get("referer")
 
-  const headersList = headers()
-  const referer = headersList.get("referer")
-
-  cookieStore.set("referer", referer ?? "")
-  
-  console.log({ headersList, referer })
-
-  redirect("/download")
+  if (token?.value.includes("vercel.com")) {
+    redirect("/download")
+  }
 }
